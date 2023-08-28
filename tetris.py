c1c14ad72c067ae538fbb0a5feb1ca05283e9cf7
jokke660 <joakim.2@hotmail.com>
2023-08-28 15:24:56 +0200

    endret kommentarer i tetris.py

--- tetris.py
+++ tetris.py
@@ -18,7 +18,7 @@ tetrominoer = [
 
 # Spilllogikk
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)       # Definer fargepar
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)       # Definerer fargepar
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
